f6b9d24f596982f46eb5400e3c7d5914ae33e194
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-23 12:01:03 +0100

    test(notebook): update test

--- spec/notebook_spec.rb
+++ spec/notebook_spec.rb
@@ -1,11 +1,15 @@
+require 'notebook'
 require 'note'
 
 describe Notebook do
   describe "#add" do
-    note = Note.new("This is the title", "and this is the body")
-
-    subject.add("This is the title", "and this is the body")
-
-    expect(subject.notes).to include note
+    it "adds a new note to the notebook" do
+      subject.add("This is the title", "and this is the body")
+      subject.notes.each do |note|
+        expect(note).to be_a(Note)
+        expect(note.title).to eq "This is the title"
+        expect(note.body).to eq "and this is the body"
+      end
+    end
   end
 end
