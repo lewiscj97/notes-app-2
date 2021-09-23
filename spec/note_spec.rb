2355fb3b79286396588003b481f1cec055b0dccb
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-23 11:42:24 +0100

    test(note): add first failing test for note class

---
+++ spec/note_spec.rb
@@ -0,0 +1,7 @@
+describe Note do
+  it "creates a new note with a title and a body" do
+    note = Note.new("This is the title", "And this is the body")
+    expect(note.title).to eq "This is the title"
+    expect(note.body).to eq "And this is the body"
+  end
+end
